c275a3ae184a6fc012049efb0a2545ddd5feba77
hecoter <92445963+hecoter@users.noreply.github.com>
2022-04-24 17:24:08 +0800

    Delete yangshipin directory

--- yangshipin/flowid.js
+++
@@ -1,8 +0,0 @@
-
-
-function get_flowid() {
-    flowid = `${(new Date).getTime().toString(36)}_${Math.random().toString(36).replace(/^0./, "")}`
-    console.log(flowid)
-    return flowid
-}
-get_flowid()
